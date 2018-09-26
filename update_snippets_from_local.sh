9bf7a80df390d5c8e7e7d8a6014cc7a711ba8841
Ro_bber <ro_bber@126.com>
2018-09-26 12:15:45 +0800

    update Code Snippets

--- update_snippets_from_local.sh
+++ update_snippets_from_local.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 SRC_HOME=`pwd`
-mv ${SRC_HOME}/CodeSnippets ${SRC_HOME}/CodeSnippets.backup
-ln -s ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/CodeSnippets
+#mv ${SRC_HOME}/CodeSnippets ${SRC_HOME}/CodeSnippets.backup
+rm -rf ${SRC_HOME}/CodeSnippets/*
+cp -f ~/Library/Developer/Xcode/UserData/CodeSnippets/* ${SRC_HOME}/CodeSnippets/
+#ln -s ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/CodeSnippets
 echo "done"
